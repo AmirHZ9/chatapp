6b94ec9975a267035838a491fe31d31682cace8e
Amirhossein zanganeh <amirzanganeh1999@gmail.com>
2021-10-20 13:55:17 +0330

    chatengine

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { auth } from '../Firebase'
 import { useHistory } from 'react-router-dom'
+import {ChatEngine} from 'react-chat-engine'
 import Nav from './Nav'
 function Chats() {
     const history = useHistory()
@@ -11,6 +12,13 @@ function Chats() {
     return (
         <div>
             <Nav logoutHandler={logoutHandler} />
+
+            <ChatEngine
+                height="calc(100vh-50px)"
+                projectID="0bfdaf2c-99bb-4d74-a9d1-9c7b3d4d91c2"
+                userName='.0'
+                userSecret="."
+            />
         </div>
     )
 }
